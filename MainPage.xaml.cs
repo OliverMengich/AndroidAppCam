61f698629542843d1d6e31c9517161b04cbbb877
OliverMengich <kemeioliver2000@gmail.com>
2020-04-21 17:06:36 +0300

    Connect with camera Android app

---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+
+namespace CBCamera
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if(!(CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported))
+            {
+                await DisplayAlert("No Camera", "No Camera Available", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory="Sample",
+                Name="Test.jpg"
+            });
+
+            if (file == null)
+                return;
+            await DisplayAlert("File Location",file.Path,"OK");
+
+            image.Source = ImageSource.FromStream(() =>
+              {
+                  var stream = file.GetStream();
+                  file.Dispose();
+                  return stream;
+              });
+        }
+        
+    }
+}
